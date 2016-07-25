8b26da5e5be0283b6bf0a99a3cda03e002e818bf
nolanbconaway <nolanbconaway@gmail.com>
2016-07-25 16:14:29 -0400

    animation

--- traingif.r
+++ traingif.r
@@ -10,21 +10,26 @@ df = read.csv('csv/switchtrain.csv')
 df = subset(df,attempt_num==1)
 
 
-saveGIF({
-for (b in c(1:12,12,12,12)) {
+for (c in 2:4) {
+	fname = paste0('type',toString(c),'.gif')
+	saveGIF( {
+		for (b in c(1:12,12,12,12)) {
+			IND = (df$shj==c) & (df$block==b)
+			data = pairwise( df$start[IND], df$finish[IND]  )
+			classlabels = assignments[,c]
 
-	par(mfrow=c(1,3))
-	for (c in 2:4) {
-		IND = (df$shj==c) & (df$block==b)
-		data = pairwise( df$start[IND], df$finish[IND]  )
-		classlabels = assignments[,c]
 
+			par(mgp=c(1,0.05,0), tcl=-0, mar=c(2,2,1.9,2))
+			plotpairwise(data, classlabels, sorting=T, LIM = 0.4)
+			title(main = paste0('Type ',  toString(c), ' Block ', toString(b)), cex=2)
+			}
+		}, 
+			ani.width = 300, ani.height = 300, 
+			interval = 0.6, 
+			movie.name = fname
+	)
 
-		par(mgp=c(1,0.05,0), tcl=-0, mar=c(2,2,1.8,2))
-		plotpairwise(data, classlabels, sorting=T, LIM = 0.4)
-		title(main = paste0('Type ',  toString(c), ' Block ', toString(b)), cex=2)
-	}
 }
-}, ani.width = 750, ani.height = 250, interval = 0.5,
-)
+
+
 
